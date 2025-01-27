cb7ebbc1abca7a495187d3da4c191e4b54e6332d
Gabriel Mattos <43770191+pretolio@users.noreply.github.com>
2025-01-27 19:07:02 -0300

    aumento de timeout

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const next = require('next');
 const axios = require('axios');
 const xml2js = require('xml2js');
+const timeout = require('connect-timeout'); // Adicionar o middleware timeout
 
 const dev = process.env.NODE_ENV !== 'production';
 const app = next({ dev });
@@ -15,6 +16,9 @@ app.prepare().then(() => {
     // Aumentar o limite de tamanho do JSON
     server.use(express.json({ limit: '50mb' }));
     server.use(express.urlencoded({ limit: '50mb', extended: true }));
+
+    // Configurar o timeout para 30 segundos
+    server.use(timeout('30s'));
 
     server.all('*', (req, res) => {
         return handle(req, res);
